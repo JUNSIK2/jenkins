fc66541d845b1711a39cb814cb0d672ca9125dd8
JJH <arc551234@gmail.com>
2021-12-21 16:55:51 +0900

    Add IndexController

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
-    @GetMapping("/hello")
+    @GetMapping("/")
     public String hello() {
         return "Hello, Jenkins!!!";
     }
